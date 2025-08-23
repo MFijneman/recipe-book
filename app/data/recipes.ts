2ccc012edb1d676fbb2d8f3771082b3496e00fe5
Mark Fijneman <mark.fijneman@momomedical.com>
2025-08-23 19:45:02 +0200

    feat: add them to the homepage

--- app/data/recipes.ts
+++ app/data/recipes.ts
@@ -1,5 +1,13 @@
+import bifteki from './recipes/bifteki';
 import coqAuVin from './recipes/coqAuVin';
+import onePanOrzo from './recipes/onePanOrzo';
+import ricottaSpinachRavioli from './recipes/ricottaSpinachRavioli';
+import traybakeGnocchi from './recipes/traybakGnocchi';
 
 export const recipes = [
-  coqAuVin
+  coqAuVin,
+  bifteki,
+  onePanOrzo,
+  ricottaSpinachRavioli,
+  traybakeGnocchi,
 ];
